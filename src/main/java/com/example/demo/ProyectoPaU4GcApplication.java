e11e4e63d940fd85f83af0309c47b86b8272d384
GeoWillC <gwconlago@uce.edu.ec>
2023-02-24 19:45:36 -0500

    Taller 32

---
+++ src/main/java/com/example/demo/ProyectoPaU4GcApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPaU4GcApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPaU4GcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+//		() -> {
+//			System.out.println("Hola mundo");
+//		}
+	}
+}
